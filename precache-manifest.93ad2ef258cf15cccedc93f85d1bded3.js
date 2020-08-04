683449e3c5917e690b08ac266b3ed746d34c40d6
Taylor Epskamp <taylorepskamp@gmail.com>
2020-08-04 14:37:47 +0200

    Updates

---
+++ precache-manifest.93ad2ef258cf15cccedc93f85d1bded3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162402fc2fb6086238590d2459080570",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "5447b6550d201380a43e",
+    "url": "/quiz/static/js/2.2c5a2655.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz/static/js/2.2c5a2655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd3a0679aac44a23287",
+    "url": "/quiz/static/js/main.26d576b1.chunk.js"
+  },
+  {
+    "revision": "1e391d41ddb7f733dbeb",
+    "url": "/quiz/static/js/runtime-main.0f5f6437.js"
+  },
+  {
+    "revision": "37346eb820052a8c74a997ecd69f578d",
+    "url": "/quiz/static/media/background.37346eb8.jpg"
+  }
+]);
